1ed880dd6b43c8f6881b31057f54106290b091ea
Marek Fajkus <marek.faj@gmail.com>
2019-04-27 21:39:25 +0200

    add documentation

--- src/Lazy/Tree.elm
+++ src/Lazy/Tree.elm
@@ -33,7 +33,7 @@ to lazily evaluate levels of Tree.
 
 # Transforms
 
-@docs map, map2, filter, filterMap, sort, sortBy, sortWith, andMap, flatten, andThen
+@docs map, map2, filter, filterMap, sort, sortBy, sortWith, andMap, flatten, andThen, duplicate, extend
 
 
 # Forest
